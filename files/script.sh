26ea227f00cadff867c3001d2df6a1e385bf0191
Alberto Oliveira <alberto.j.oliveira@parceiros.nos.pt>
2025-07-04 10:25:19 +0100

    Update script.sh

--- files/script.sh
+++ files/script.sh
@@ -5,7 +5,7 @@ COMMIT_SHA="$2"
 BRANCH_NAME="$3"
 EVENT_NAME="$4"
 
-echo "### Summary" >> $GITHUB_STEP_SUMMARY
+echo "## Summary" >> $GITHUB_STEP_SUMMARY
 echo "- **Job Name:** $JOB_NAME" >> $GITHUB_STEP_SUMMARY
 echo "- **Commit SHA:** $COMMIT_SHA" >> $GITHUB_STEP_SUMMARY
 echo "- **Branch:** $BRANCH_NAME" >> $GITHUB_STEP_SUMMARY
